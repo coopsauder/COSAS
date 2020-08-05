56abe9c480e8b335480a9c4cb426f08c513c6cc7
watshever <louieweng@outlook.com>
2020-08-05 15:05:16 -0400

    added some fade animations

--- static_files/scripts/scripts.js
+++ static_files/scripts/scripts.js
@@ -14,5 +14,14 @@ $(document).ready(function() {
     $('.email').on('click', function() {
         $('.email').popover('toggle')
     })
+
+    ScrollReveal().reveal('.nav-link', { duration: 1000, interval: 300 })
+    ScrollReveal().reveal('.home', { duration: 5000 })
+    ScrollReveal().reveal('.about', { duration: 3000 })
+    ScrollReveal().reveal('.initiatives', { duration: 4000, interval: 500 })
+    ScrollReveal().reveal('.events', { duration: 3000, interval: 250 })
+    ScrollReveal().reveal('.team', { duration: 3000, interval: 250 })
+    ScrollReveal().reveal('.contact', { duration: 3000 })
 })
     
+
