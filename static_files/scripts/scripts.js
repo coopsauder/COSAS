7f67751749ce5567ba3eafa2ebcff509c74d0a63
watshever <louieweng@outlook.com>
2020-08-06 13:29:53 -0400

    removed nav fade

--- static_files/scripts/scripts.js
+++ static_files/scripts/scripts.js
@@ -15,13 +15,12 @@ $(document).ready(function() {
         $('.email').popover('toggle')
     })
 
-    ScrollReveal({ mobile:false }).reveal('.navigation', { duration: 1000, interval: 200 })
-    ScrollReveal().reveal('.home', { duration: 5000 })
+    ScrollReveal().reveal('.home', { duration: 3000 })
     ScrollReveal().reveal('.about-scene', { duration: 3000 })
     ScrollReveal().reveal('.about', { duration: 5000 })
     ScrollReveal().reveal('.initiatives', { duration: 4000, interval: 300 })
     ScrollReveal().reveal('.events', { duration: 3000, interval: 250 })
-    ScrollReveal().reveal('.team', { duration: 3000, interval: 250 })
+    ScrollReveal().reveal('.team', { duration: 2000, interval: 150 })
     ScrollReveal().reveal('.contact', { duration: 3000 })
 })
     
